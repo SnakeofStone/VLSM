b13cca9985d42b9ea22582767c22dd387687877c
Ruben Curiel <jorgeruben99@gmail.com>
2021-10-08 16:26:54 -0500

    Improve visualization
    Signed-off-by: Ruben Curiel <jorgeruben99@gmail.com>

--- gui.py
+++ gui.py
@@ -4,8 +4,8 @@ class OutputTable:
     def __init__(self, root: tkinter.Tk, data: list):
         for i in range(len(data)):
             for j in range(len(data[0])):
-                self.e = tkinter.Entry(root, width=20, 
-                                       font=('Arial', '16', 'bold'))
+                self.e = tkinter.Entry(root, width=18, 
+                                       font=('Arial', '12', 'bold'))
 
                 self.e.grid(row=i, column=j)
                 self.e.insert(tkinter.END, data[i][j])
